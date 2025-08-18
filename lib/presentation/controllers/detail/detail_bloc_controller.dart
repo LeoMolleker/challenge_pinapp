1bbc16b79bba7d37cca3bc16e74e6ee8c8b3119e
Leonardo Molleker <molleker.l.m@gmail.com>
2025-08-17 21:29:21 -0300

    readme

--- lib/presentation/controllers/detail/detail_bloc_controller.dart
+++ lib/presentation/controllers/detail/detail_bloc_controller.dart
@@ -1,4 +1,3 @@
-import 'package:challenge_pinapp/domain/entities/failure.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../domain/use_cases/get_post_comments_use_case.dart';
